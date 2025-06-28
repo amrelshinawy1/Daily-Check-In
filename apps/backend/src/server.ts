21d22af3182569510b854d6ad4acef38a219fe5b
amr.elshinawy <amrelshinawy1@gmail.com>
2025-06-28 14:39:31 +0300

    fix run dev

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,5 +1,5 @@
 import { app } from './app';
-import config from './config/env';
+import { config } from './config/env';
 
 app.listen(config.PORT, () => {
   console.log(`🚀 Server running on port ${config.PORT}`);
